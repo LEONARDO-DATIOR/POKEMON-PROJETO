53ed7f38b8b01a6699b9018c71321926ef9e794d
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2025-11-03 02:01:59 -0300

    Teste de consumo da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
+function Pokedex() {
+  const [personagensHP, setpersonagensHP] = useState([]);
+
+
+  useEffect(() => {
+    const fetchPersonagensHP = async () => {
+      const personagensAPI = await fetch('https://hp-api.onrender.com/api/characters')
+      const data = await personagensAPI.json()
+      setpersonagensHP(data)
+    }
+
+    fetchPersonagensHP();
+  }); 
+
 
   return (
     <>
-      
+      <h1>Todos os personagens: </h1>
+      {
+        personagensHP.map((personagens, index) => {
+          return (
+            <>
+            {
+              personagens.actor ?
+              <div key={index} >
+                <h2 >{personagens.name}</h2>
+                {personagens.imagem ? <></> : <img  src={personagens.image} alt="" /> }
+                <p>{personagens.actor}</p>
+              </div>
+              :
+              ""
+            }
+            </>
+          );
+        })
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default Pokedex;
